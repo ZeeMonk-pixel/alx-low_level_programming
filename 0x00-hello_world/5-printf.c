f30ddcb51968d2a200e00cdd15d12e30066f3828
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-16 10:39:07 -0800

    low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main(void)- function to print the text
+ * prints with proper grammar, but the outcome is a piece of art, to the terminal
+ * Return: returns 0.
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
