2ca301519b1d47bfe7d39b415e55a59eeeaf8926
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-16 11:19:33 -0800

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(msg) - 1);
+	char msg[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(msg));
+
 	return (1);
 }
