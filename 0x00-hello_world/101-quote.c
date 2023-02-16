c70578cf34e12045eb86518addb2f2fb3b028a64
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-16 11:09:27 -0800

    low level programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main(void)- prints message to standard error,
+ * writesmessage to standard error
+ * Return: returns 1
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(msg) - 1);
+	return (1);
+}
