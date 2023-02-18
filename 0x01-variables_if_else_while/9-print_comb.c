55721065d8309e1569b40984d172abea9207fdd6
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-18 02:36:39 -0800

    c variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main(void)-prints all possible combinations of single-digit numbers.
+ * prints in ascending order
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(', ');
+			putchar(j + '0');
+			putchar('\n');
+		}
+	}
+	return (0);
+}
