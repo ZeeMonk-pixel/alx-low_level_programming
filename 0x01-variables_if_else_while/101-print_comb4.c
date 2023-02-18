3f7c03d6921367602045c741f7cf7739d3a63bd8
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-18 07:29:08 -0800

    c-variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main(void)- prints all possible different combinations of two digits
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+			}
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
