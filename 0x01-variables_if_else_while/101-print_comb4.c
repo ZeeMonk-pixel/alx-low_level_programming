e3791f529231b1c8527e8ed6c60544b42bc54388
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-18 07:40:18 -0800

    c variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,16 +13,16 @@ int main(void)
 	{
 		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k < 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar('0' + i);
 				putchar('0' + j);
 				putchar('0' + k);
-			}
-			if (i != 7 || j != 8 || k != 9)
-			{
-				putchar(',');
-				putchar(' ');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
