ed7fff3b2f16378c8d885ef27124bdf8edc70b42
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-21 17:04:42 -0800

    nested functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,42 +7,34 @@
  */
 void print_times_table(int n)
 {
-	int i, j, result;
+	int i, j;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				result = i * j;
-				if (j == 0)
+				int result = i * j;
+
+				_putchar(',');
+				_putchar(' ');
+				if (result < 100)
 				{
-					_putchar('0');
+					_putchar(' ');
 				}
-				else if (result < 10)
+				if (result < 10)
 				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
 				}
-				else if (result < 100)
+				if (result >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result / 10 + '0');
-					_putchar(result % 10 + '0');
+					_putchar((result / 100) + '0');
 				}
-				else
+				if (result >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(result / 100 + '0');
-					_putchar((result / 10) % 10 + '0');
-					_putchar(result % 10 + '0');
+					_putchar(((result / 10)) % 10 + '0');
 				}
 			}
 			_putchar('\n');
