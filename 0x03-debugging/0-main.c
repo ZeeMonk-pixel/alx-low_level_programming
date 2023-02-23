558229ee19be7400df20ae178a152e7784ad8765
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-23 02:36:32 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main(void)- test that the function positive_or_negative()
+ *  gives the correct output when given a case of 0
+ *  Return: returns 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
