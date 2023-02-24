dd52b3a58ee9e826b00ce1633caeddf12bd58f4e
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-24 07:42:47 -0800

    nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,11 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
+        if (n <= 0)
+        {
+                _putchar('\n');
+                return;
+        }
 	int i;
 
 	for (i = 0; i < n; i++)
