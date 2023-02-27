75dc6477d639aaa87c870da0811ff82d44072996
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-02-27 06:02:18 -0800

    pointers_arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The string to print
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int n = 0;
+	int i = n;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2 + 1;
+	}
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
