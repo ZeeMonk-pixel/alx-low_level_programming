85dbc13e656730eadeaf98be5331547e4b43245a
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-03-03 02:27:17 -0800

    pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @*dest: first string
+ * @*src: second string
+ * Return: returns dest
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		*p++ = *src++;
+	}
+	*p = '\0';
+	return (dest);
+}
