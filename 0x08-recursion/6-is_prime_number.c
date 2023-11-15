bc62b1dd18edb68e613b5f6ca36fae6fb5d749a1
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-11-15 06:51:01 -0800

    bug fixes with betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,6 @@
 
 /**
  * is_divisible - check if a number is divisible by something
- * is_prime_number - checks if a number is prime
  * @n: first int
  *@divisor: divisor
  * Return: Always 0.
@@ -19,6 +18,12 @@ int is_divisible(int n, int divisor)
 	}
 	return (is_divisible(n, divisor - 1));
 }
+
+/**
+ * is_prime_number - checks for prime numberss
+ * @n: integer
+ * Return: Always 0
+ */
 
 int is_prime_number(int n)
 {
