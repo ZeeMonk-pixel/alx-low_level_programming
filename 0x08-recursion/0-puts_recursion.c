50a9a3906a3fa72a29a7803c4a7065b135f46266
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2023-11-15 01:54:42 -0800

    work on betty styles

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
+/**
+ * _puts_recursion(char *s) - tkes a string and performs a recursive function on them
+ *
+ * Return: Always 0.
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
-	}
-	
+	}	
 	_putchar(*s);
 
 	_puts_recursion(s + 1);
